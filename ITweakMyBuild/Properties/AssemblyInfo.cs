d105b66927932a5ce7ea377bf1dcadca8b9dd6f4
tom-englert <mail@tom-englert.de>
2016-10-09 17:12:57 +0200

    V 1.0.1.0

--- ITweakMyBuild/Properties/AssemblyInfo.cs
+++ ITweakMyBuild/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ namespace ITweakMyBuild.Properties
 {
     internal static class Product
     {
-        public const string Version = "1.0.0.0";
+        public const string Version = "1.0.1.0";
     }
 }
